f5059d5b59e1fd0c98fce3e1f48045740d979717
beMang <antonuttiadrien@gmail.com>
2018-05-19 12:10:04 +0200

    adding file for tests

---
+++ src/Config.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace bemang\Core;
+
+class Config
+{
+    protected $config = [];
+    protected $configFiles;
+
+    public function getConfigArray()
+    {
+        return $this->config;
+    }
+
+    public function get($key)
+    {
+        if (!empty($this->config[$key])) {
+            return $this->config[$key];
+        } else {
+            return false;
+        }
+    }
+
+    public function define($file)
+    {
+        if (is_file($file)) {
+            $this->config = array_merge($this->config, require($file));
+        } else {
+            return false;
+        }
+    }
+
+    public function defineUnique($nameParam, $valParam)
+    {
+        $this->config[$nameParam] = $valParam;
+    }
+}

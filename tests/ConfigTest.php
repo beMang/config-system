97867ce852dafa7a53e626acb13f77f9b0c38f18
beMang <antonuttiadrien@gmail.com>
2019-05-15 15:52:35 +0200

    update packages (phpunit)

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -8,7 +8,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
 {
     protected $configInstance;
 
-    public function setUp()
+    public function setUp() :void
     {
         require_once(__DIR__ . '/../vendor/autoload.php');
         $this->configInstance = Config::getInstance();
